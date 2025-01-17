c7cd611fb4aa3e9494ae48d4b861451387e53bba
ZAHRAMG <zahra.mguitaa.contact@gmail.com>
2025-01-17 17:27:21 +0100

    feat: change project structure

--- app/ui/Text/index.tsx
+++ app/ui/Text/index.tsx
@@ -1,4 +1,4 @@
-import { TextProps } from "@/components/Text/types";
+import { TextProps } from "@/app/ui/Text/types";
 
 export default function Text(props: TextProps) {
   return <p className={`${props.bold && "font-bold"}`}>{props.text}</p>;
